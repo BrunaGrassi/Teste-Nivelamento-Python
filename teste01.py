5594219e68842c9128bab308c5ca0c60d1cea41e
Bruna Grassi <101432594+BrunaGrassi@users.noreply.github.com>
2024-11-27 21:25:19 -0300

    solução
    solução 1

---
+++ teste01.py
@@ -0,0 +1,7 @@
+# Parte 1
+# Conceitos Básicos --> Escreva um código que peça ao usuário dois números e exiba a soma deles.
+
+n1 = int(input("Digite um número: "))
+n2 = int(input("Digite outro número para somar: "))
+
+print(f"A soma dos números é: {(n1 + n2)}")
