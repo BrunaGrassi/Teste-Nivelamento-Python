cca6cc34e4e07210e8a4277f74b55165828f1a61
Bruna Grassi <101432594+BrunaGrassi@users.noreply.github.com>
2024-12-03 15:43:29 -0300

    solução

--- teste02.py
+++ teste02.py
@@ -1,2 +1,9 @@
 # Parte 1 - Conceitos Básicos
 # Exercício 2 --> Escreva um programa que leia uma lista de números e exiba apenas os números pares.
+
+entrada = input("Digite uma lista de números inteiros: ") 
+
+# utilizamos o map() para converter cada elemento para inteiro; o split() para separar os elementos da lista utilizando o ' ' (espaço em branco) como divisor;
+lista = list(map(int, entrada.split()))
+
+print(lista)
