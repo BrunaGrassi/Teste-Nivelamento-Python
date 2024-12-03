e07034e9f82b87507b8e737e8320f535a95fe0e6
Bruna Grassi <101432594+BrunaGrassi@users.noreply.github.com>
2024-12-03 15:26:35 -0300

    adicionando validação
    # validando se a entrada do usuário é um número inteiro

--- teste01.py
+++ teste01.py
@@ -1,7 +1,10 @@
-# Parte 1
-# Conceitos Básicos --> Escreva um código que peça ao usuário dois números e exiba a soma deles.
+# Parte 1 - Conceitos Básicos
+# Exercício 1 --> Escreva um código que peça ao usuário dois números e exiba a soma deles.
 
-n1 = int(input("Digite um número: "))
-n2 = int(input("Digite outro número para somar: "))
+try:
+    n1 = int(input("Digite um número: "))
+    n2 = int(input("Digite outro número para somar: "))
 
-print(f"A soma dos números é: {(n1 + n2)}")
+    print(f"A soma dos números é: {(n1 + n2)}")
+except ValueError:
+    print("Insira apenas números inteiros, por favor.")
---
+++ teste02.py
@@ -0,0 +1,2 @@
+# Parte 1 - Conceitos Básicos
+# Exercício 2 --> Escreva um programa que leia uma lista de números e exiba apenas os números pares.
