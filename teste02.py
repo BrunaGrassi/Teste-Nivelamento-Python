3c22205fe2fd24923fc9a03d246c0a95cc458139
Bruna Grassi <101432594+BrunaGrassi@users.noreply.github.com>
2024-12-03 16:15:02 -0300

    segunda parte da resolução
    filtrando os pares e ímpares e criando novas listas

--- teste02.py
+++ teste02.py
@@ -1,9 +1,16 @@
 # Parte 1 - Conceitos Básicos
 # Exercício 2 --> Escreva um programa que leia uma lista de números e exiba apenas os números pares.
 
-entrada = input("Digite uma lista de números inteiros: ") 
+entrada = input("Digite uma lista de números inteiros separados por espaço em branco: ") 
 
 # utilizamos o map() para converter cada elemento para inteiro; o split() para separar os elementos da lista utilizando o ' ' (espaço em branco) como divisor;
 lista = list(map(int, entrada.split()))
 
-print(lista)
+def numeroPar(numero):
+    return numero % 2 == 0
+
+pares = list(filter(numeroPar, lista))
+impares = list(filter(lambda x: not numeroPar(x), lista))
+    
+print(f"Os números pares são: {pares}")
+print(f"Os números ímpares são: {impares}")
